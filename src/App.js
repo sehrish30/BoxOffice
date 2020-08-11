7e1d77cce679917215ffdb63c253c244b8bea9c5
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-08-11 14:19:33 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Starred from './pages/Starred';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
-      <Route exact path="/starred">
-        This is starred page
-      </Route>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>This a 404 page</Route>
-    </Switch>
+        <Route>
+          <div>Not Found</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/pages/Starred.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Starred = props => {
+  return <div>This is starred</div>;
+};
+
+Starred.propTypes = {};
+
+export default Starred;
