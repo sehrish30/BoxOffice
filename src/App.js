5bc0207e2b3ed5b33d2bd699131ffd6d597e4ff3
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-08-11 13:49:23 +0300

    REact router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route exact path="/starred">
+        This is starred page
+      </Route>
+
+      <Route>This a 404 page</Route>
+    </Switch>
   );
 }
 
