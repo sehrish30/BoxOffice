9fed3429eb9229814313b11fb88b3c1c72dc8926
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-08-12 19:03:03 +0300

    Updates

---
+++ precache-manifest.43ca49ffbff5854f0125a9a8688b5730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa02b56c47980d931ebd8a21275fef5",
+    "url": "/BoxOffice/index.html"
+  },
+  {
+    "revision": "e16d042920e469af6a04",
+    "url": "/BoxOffice/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "a4dd3ba7c2fa05f3c4e0",
+    "url": "/BoxOffice/static/js/2.ae45aaa8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BoxOffice/static/js/2.ae45aaa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e16d042920e469af6a04",
+    "url": "/BoxOffice/static/js/main.b2c5eb3c.chunk.js"
+  },
+  {
+    "revision": "9d4bd0cd7dd104df2d2e",
+    "url": "/BoxOffice/static/js/runtime-main.09a061e4.js"
+  }
+]);
